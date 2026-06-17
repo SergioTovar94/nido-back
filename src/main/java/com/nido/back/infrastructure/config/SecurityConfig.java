43443af0c17888ebfad75a7be20d955ece93b4fb
SergioTovar94 <sergiotovar9430@gmail.com>
2026-06-17 16:30:19 -0500

    Modified

--- src/main/java/com/nido/back/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/nido/back/infrastructure/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.nido.back.infrastructure.security.CustomUserDetailsService;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
